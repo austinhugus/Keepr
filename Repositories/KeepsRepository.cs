42b73f74c02cb2a3467f44bc3a53220e057abcc7
austin <ap.hugus@gmail.com>
2020-07-17 17:10:27 -0600

    KeepsService works

--- Repositories/KeepsRepository.cs
+++ Repositories/KeepsRepository.cs
@@ -1,29 +1,53 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using Keepr.Models;
 using Dapper;
+using Keepr.Models;
 
-namespace Keepr.Repositories
-{
-    public class KeepsRepository
-    {
+namespace Keepr.Repositories {
+    public class KeepsRepository {
         private readonly IDbConnection _db;
 
-        public KeepsRepository(IDbConnection db)
-        {
+        public KeepsRepository (IDbConnection db) {
             _db = db;
         }
 
-        internal IEnumerable<Keep> Get()
-        {
+        internal IEnumerable<Keep> Get () {
             string sql = "SELECT * FROM Keeps WHERE isPrivate = 0;";
-            return _db.Query<Keep>(sql);
+            return _db.Query<Keep> (sql);
         }
 
-        internal Keep Create(Keep KeepData)
-        {
-            throw new NotImplementedException();
+        internal Keep Create (Keep newKeep) {
+            string sql = @"
+            INSERT INTO keeps
+            ()
+            
+            
+            "
+        }
+
+        internal object GetKeepsByUserId (string userId) {
+
+        }
+
+        internal Keep GetById (int id) {
+
+        }
+
+        internal bool ViewKeep (Keep keepToUpdate) {
+
+        }
+
+        internal bool ShareKeep (int id) {
+
+        }
+
+        internal bool Edit (Keep keepToUpdate, string userId) {
+
+        }
+
+        internal bool Delete (int id, string userId) {
+
         }
     }
 }
--- Services/KeepsService.cs
+++ Services/KeepsService.cs
@@ -19,19 +19,43 @@ namespace Keepr.Services {
         }
 
         internal object GetByUserId (string userId) {
+            return _repo.GetKeepsByUserId (userId);
+        }
+
+        internal Keep GetById (int id) {
+            Keep foundKeep = _repo.GetById (id);
+            if (foundKeep == null) {
+                throw new Exception ("Invalid Id");
+            }
+            return foundKeep;
+        }
+
+        internal Keep Edit (Keep keepToUpdate, string userId) {
+            Keep foundKeep = GetById (keepToUpdate.Id);
+            if (_repo.ViewKeep (keepToUpdate)) {
+                foundKeep.Views = keepToUpdate.Views;
+                return foundKeep;
+            }
+            if (_repo.ShareKeep (keepToUpdate.Id)) {
+                foundKeep.Shares = keepToUpdate.Keeps;
+                return foundKeep;
+            }
+            if (foundKeep.UserId == userId && _repo.Edit (keepToUpdate, userId)) {
+                return keepToUpdate;
+            }
+            throw new Exception ("Can't share your own Post");
 
         }
 
-        internal object GetById (int id) {
-
-        }
-
-        internal object Edit (Keep keepToUpdate, string userId) {
-
-        }
-
-        internal object Delete (int id, string userId) {
-
+        internal string Delete (int id, string userId) {
+            Keep foundKeep = GetById (id);
+            if (foundKeep.UserId != userId) {
+                throw new Exception ("This is not your car!");
+            }
+            if (_repo.Delete (id, userId)) {
+                return "Successfully deleted";
+            }
+            throw new Exception ("Not Successfully Deleted");
         }
     }
 }
